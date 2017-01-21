11f1067be724a1ba64beee32eec21ce8d0cb38f2
Dave Yates <daveyates@iMac.home>
2017-01-21 16:46:20 +0000

    fixed issue with the api authorization. IF I HAD MADE A TEST FOR THAT THIS WOULDN'T OF HAPPENED >_>

--- api-starter/app/commands/authorize_api_request.rb
+++ api-starter/app/commands/authorize_api_request.rb
@@ -1,7 +1,7 @@
-class AuthorizeApiRequest
   prepend SimpleCommand
   def initialize(headers = {})
     @headers = headers
+class AuthorizeApiRequest
   end
 
   def call
@@ -13,7 +13,7 @@ class AuthorizeApiRequest
   attr_reader :headers
 
   def user
-    @user ||= user.find(decoded_auth_token[:user_id]) if decoded_auth_token
+    @user ||= User.find(decoded_auth_token[:user_id]) if decoded_auth_token
     @user || errors.add(:token, 'invalid token') && nil
   end
   def decoded_auth_token
